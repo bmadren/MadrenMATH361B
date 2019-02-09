0e398a41078e5d851a3f037cfc8b246e27605c93
benmadren <bmadren@mail.umw.edu>
2019-02-08 19:47:56 -0500

    Edited partial sum program and pdf

--- IntroToProgramming/PartialSum_Madren.py
+++ IntroToProgramming/PartialSum_Madren.py
@@ -5,37 +5,37 @@ Created on Mon Feb  4 19:45:29 2019
 @author: benma
 """
 #%%
+import numpy as np
 from math import log
-N = 30
-sum=0
-sums=[]
-
-for nn in range(1,N+1):
-    sum += (log(nn**4+nn+1)**2)/((nn)**(1/2)+3)
-    sums.append(sum)
+N = 50
+x = np.zeros([N])
+term = 0
+for nn in range(1, N):
+    term += (log(nn**4+nn+1)**2)/((nn)**(1/2)+3)
+    x[nn] = term
     
-print("The terms are ", sums)
+print("The first 15 terms are ", x[:15], " and the last 15 terms are ", x[-15:])
 
 #%%
+import numpy as np
 from math import exp
-N=30
-sum=0
-sums=[]
-
-for nn in range(1,N+1):
-    sum += ((exp(nn/100))/(nn**10))
-    sums.append(sum)
+N = 50
+x = np.zeros([N])
+term = 0
+for nn in range(1, N):
+    term += ((exp(nn/100))/(nn**10))
+    x[nn] = term
     
-print("The terms are ", sums)
+print("The first 15 terms are ", x[:15], " and the last 15 terms are ", x[-15:])
 
 #%%
+import numpy as np
 from math import exp
-N=30
-sum=0
-sums=[]
-
-for nn in range(1,N+1):
-    sum += ((nn+1)**2)/(exp(nn))
-    sums.append(sum)
+N = 50
+x = np.zeros([N])
+term = 0
+for nn in range(1, N):
+    term += ((nn+1)**2)/(exp(nn))
+    x[nn] = term
     
-print("The terms are ", sums)
+print("The first 15 terms are ", x[:15], " and the last 15 terms are ", x[-15:])
