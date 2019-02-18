0f34e7ff8d371487e9ead3ed6452e402d1fc9559
benmadren <bmadren@mail.umw.edu>
2019-02-18 12:27:15 -0500

    Edited PW Plot

--- IntroToProgramming/PWPlot_Madren.py
+++ IntroToProgramming/PWPlot_Madren.py
@@ -10,24 +10,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 N = 10
-def function1(x):
-    if x < -2:
-        y = -3*(x+2)**2 + 1
-    elif -2 <= x < -1:
-        y = 1
-    elif -1 <= x <= 1:
-        y = (x-1)**3 + 3
-    elif 1 < x < 2:
-        y = np.sin(np.pi*x) + 3
-    else:
-        y = 3*(np.sqrt(x-2)) + 4
-    return y
+def PWFunc(x):
+        if (x < -2):
+            y = -3*(x+2)**2 + 1
+        elif (-2 <= x and x < -1):
+            y = 1
+        elif (-1 <= x and x <= 1):
+            y = (x-1)**3 + 3
+        elif (1 < x and x < 2):
+            y = np.sin(np.pi*x) + 3
+        elif (x >= 2):
+            y = 3*(np.sqrt(x-2)) + 4
+        return y
 
-x = np.linspace(-3, 3, N)
-y = np.zeros(N)
-for i in range(len(x)):
-    y[i] = function1(x[i])
-plt.plot(x, y)    
+x_array = np.linspace(-3, 3, N)
+y_array = np.zeros(N)
+for i in range(len(x_array)):
+    y_array[i] = PWFunc(x_array[i])
+plt.plot(x_array, y_array)    
 plt.xlabel("x-values")
 plt.ylabel("y-values")
 plt.title("Piecewise Function")
